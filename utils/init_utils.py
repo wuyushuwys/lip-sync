615f36145708187ffeef79fe29f87e96e7e45b97
wuyushuwys <wuyushuwys@outlook.com>
2023-12-28 00:07:16 -0500

    multi-single-gpu node scenarios

--- utils/init_utils.py
+++ utils/init_utils.py
@@ -3,7 +3,6 @@ import argparse
 import functools
 import subprocess
 
-
 import torch
 import numpy as np
 
@@ -42,17 +41,18 @@ def init_process(args):
             # multi-task multi-gpu
             # args.world_size = int(os.environ['SLURM_NPROCS']) * ngpus_per_node
             # args.rank = int(os.environ['SLURM_PROCID']) * ngpus_per_node + local_rank  # global rank
-            if 'MASTER_PORT' in os.environ:
-                pass  # use MASTER_PORT in the environment variable
-            else:
-                # 29500 is torch.distributed default port
-                os.environ['MASTER_PORT'] = '29500'
+
             node_list = os.environ['SLURM_NODELIST']
             # addr = subprocess.getoutput(f'scontrol show hostname {node_list} | head -n1')
-            os.environ['MASTER_ADDR'] = os.environ['SLURM_LAUNCH_NODE_IPADDR']  # get master addr
-            args.node_list = node_list # All node you are using
+            args.node_list = node_list  # All node you are using
             args.job_id = os.environ["SLURM_JOB_ID"]  # get job id
             args.local_rank = local_rank
+
+            # set environs
+            os.environ['MASTER_ADDR'] = os.environ['SLURM_LAUNCH_NODE_IPADDR']  # get master addr
+            os.environ['WORLD_SIZE'] = str(args.world_size)
+            os.environ['LOCAL_RANK'] = str(local_rank)
+            os.environ['RANK'] = str(args.rank)
         else:
             args.world_size = int(os.environ["WORLD_SIZE"])
             args.rank = local_rank
