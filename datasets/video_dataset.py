706931e6b40bfeff87035d3d9117603ce6a6a2ce
wuyushuwys <wuyushuwys@outlook.com>
2024-01-07 03:10:21 -0500

    add window_range

--- datasets/video_dataset.py
+++ datasets/video_dataset.py
@@ -178,7 +178,7 @@ class FrameMelDataset(Dataset):
     def _load_lipsync_train_data(self, frame_list, audio_file):
         if self.window_range:
             window_range = self.window_range * self.video_spec['fps']
-            anchor = random.sample(range(len(frame_list) - self.window_size - window_range), 1)
+            anchor = random.choice(range(len(frame_list) - self.window_size - window_range))
             idx, false_idx = random.sample(range(anchor, anchor + window_range), 2)
         else:
             idx, false_idx = random.sample(range(len(frame_list) - self.window_size), 2)
@@ -218,7 +218,7 @@ class FrameMelDataset(Dataset):
     def _load_sync_train_data(self, frame_list, audio_file):
         if self.window_range:
             window_range = self.window_range * self.video_spec['fps']
-            anchor = random.sample(range(len(frame_list) - self.window_size - window_range), 1)
+            anchor = random.choice(range(len(frame_list) - self.window_size - window_range))
             idx, false_idx = random.sample(range(anchor, anchor + window_range), 2)
         else:
             idx, false_idx = random.sample(range(len(frame_list) - self.window_size), 2)
