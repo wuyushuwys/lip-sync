0c693fc5dae4d1b5b4919d4e346ebe72bc407e9a
wuyushuwys <wuyushuwys@outlook.com>
2024-01-18 17:31:50 -0500

    replace Namespace to omegaconf

--- datasets/utils.py
+++ datasets/utils.py
@@ -4,7 +4,7 @@ from common import Hdf5
 
 def load_from_folder(folder_tree, folder, mode, ext):
     if mode == 'image':
-        frame_list = filter(lambda x: x.endswith(ext), os.listdir(folder))
+        frame_list = list(filter(lambda x: x.endswith(ext), os.listdir(folder)))
         if len(frame_list) > 0:  # only load if frame file exist
             folder_tree[folder] = sorted(frame_list)
     elif mode == 'h5':
