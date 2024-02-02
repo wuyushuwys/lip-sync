fa7edfc574c19969c37d7acb03fab6adcc84ab65
wuyushuwys <wuyushuwys@outlook.com>
2024-02-01 22:53:57 -0500

    update

--- scripts/sb_train_vqgan.bash
+++ scripts/sb_train_vqgan.bash
@@ -32,7 +32,7 @@ printf '%s\n' "Training on ${num_gpus} GPU ${CUDA_VISIBLE_DEVICES}"
 
 srun torchrun --nproc_per_node $num_gpus --master_port $MASTER_PORT train_vqgan.py \
   --config vqgan.yml \
-  --dataset ffhq celeba \
+  --dataset ffhq celeba hdtf_images \
   --model vqgan \
   --job_dir "${job_dir}" \
   --batch_size 8 \
