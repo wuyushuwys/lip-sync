8ec87907a7e6165cb9f6018e72270d0193857bb1
wuyushuwys <wuyushuwys@outlook.com>
2024-02-04 15:21:44 -0500

    ignore args

--- losses/perceptual_loss.py
+++ losses/perceptual_loss.py
@@ -27,7 +27,7 @@ class LPIPSLoss(nn.Module):
                 source: torch.Tensor,
                 target: torch.Tensor,
                 normalize: bool = True,
-                val=False
+                val=False, **ignore_kwargs
                 ) -> torch.Tensor:
         if source.dim() > 4:
             source = torch.cat(source.unbind(2), dim=0)
@@ -128,7 +128,7 @@ class PerceptualLoss(nn.Module):
                 f'{criterion} criterion has not been supported in'
                 ' this version.')
 
-    def forward(self, x, gt, val=False):
+    def forward(self, x, gt, val=False, **ignore_kwargs):
 
         if x.dim() > 4:
             x = torch.cat(x.unbind(2), dim=0)
