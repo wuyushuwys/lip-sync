f88dd74a9cfa48647cc9d72a3be4b05189536825
wuyushuwys <wuyushuwys@outlook.com>
2024-02-08 21:53:39 -0500

    update

---
+++ scripts/pretrain_mage.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export MASTER_PORT=$(((RANDOM % 1000 + 5000)))
+num_gpus=$(nvidia-smi --list-gpus | wc -l)
+
+# Experiments
+
+dataset=ffhq
+model=mage
+
+now=$(date +'%b%d-%H')
+
+experiment_name=$1
+
+if [ -z $experiment_name ]; then
+  job_dir=runs/train_${model}_${dataset}
+else
+  job_dir=runs/train_${model}_${dataset}_${experiment_name}
+fi
+
+printf '%s\n' "Training on ${num_gpus} GPU ${CUDA_VISIBLE_DEVICES}"
+
+torchrun --nproc_per_node $num_gpus --master_port $MASTER_PORT pretrain_mage.py \
+  --config mage_pretrain.yml \
+  --dataset ffhq celeba \
+  --model vqgan \
+  --job_dir "${job_dir}" \
+  --batch_size 4 \
+  --num_workers 4 \
+  --epochs 50
