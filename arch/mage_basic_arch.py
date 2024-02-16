ec633fda87f7db4cf5bcc169764397fe04b49da7
wuyushuwys <wuyushuwys@outlook.com>
2024-02-15 19:22:59 -0500

    add flash attention support

--- arch/mage_basic_arch.py
+++ arch/mage_basic_arch.py
@@ -264,31 +264,6 @@ class Attention(nn.Module):
             return x, attn
 
 
-class Block(nn.Module):
-
-    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
-                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
-        super().__init__()
-        self.norm1 = norm_layer(dim)
-        self.attn = Attention(
-            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
-        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-        self.norm2 = norm_layer(dim)
-        mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
-
-    def forward(self, x, return_attention=False):
-        if return_attention:
-            _, attn = self.attn(self.norm1(x), return_attention)
-            return attn
-        else:
-            y, _ = self.attn(self.norm1(x))
-            x = x + self.drop_path(y)
-            x = x + self.drop_path(self.mlp(self.norm2(x)))
-        return x
-
-
 class LabelSmoothingCrossEntropy(nn.Module):
     """ NLL loss with label smoothing.
     """
