9422eae0d5aa8b4353abbdeb7174c9f0c5f3e754
wuyushuwys <wuyushuwys@outlook.com>
2024-03-08 02:16:43 -0500

    update

--- models/lipsync_model.py
+++ models/lipsync_model.py
@@ -77,11 +77,11 @@ class LipSyncModel(BasicModel):
                 sync_weight = self.criteria['sync_loss'].loss_weight
                 sync_loss = self.criteria['sync_loss'](mel, pred_y) if sync_weight != 0 else 0
 
-                weight_mask = self.mask.inverse_mask
-                weight_mask[weight_mask == 0] = 0.5
-                weight_mask = rearrange(weight_mask, '(b t) c h w -> b c t h w ', b=pred_y.size(0))
-                recon_loss = self.criteria['recon_loss'](pred_y, y, weight=weight_mask) if 'recon_loss' in self.criteria.keys() else 0
-                # recon_loss = self.criteria['recon_loss'](pred_y, y) if 'recon_loss' in self.criteria.keys() else 0
+                # weight_mask = self.mask.inverse_mask
+                # weight_mask[weight_mask == 0] = 0.5
+                # weight_mask = rearrange(weight_mask, '(b t) c h w -> b c t h w ', b=pred_y.size(0))
+                # recon_loss = self.criteria['recon_loss'](pred_y, y, weight=weight_mask) if 'recon_loss' in self.criteria.keys() else 0
+                recon_loss = self.criteria['recon_loss'](pred_y, y) if 'recon_loss' in self.criteria.keys() else 0
 
                 perceptual_loss = self.criteria['perceptual_loss'](pred_y,
                                                                    y) if 'perceptual_loss' in self.criteria.keys() else 0
