8f0b25230503b7419b3af57b12b662b67b21e31c
wuyushuwys <wuyushuwys@outlook.com>
2024-03-13 23:18:09 -0400

    fix error

--- models/mage_model.py
+++ models/mage_model.py
@@ -90,7 +90,7 @@ class MageModel(BasicModel):
             with torch.autocast(device_type="cuda", dtype=torch.float16, enabled=self.use_amp):
                 loss, g, token_all_mask = self.model(x_masked, gt=y, ref=ref, audio=audio_mel, generate=True)
 
-                if len(self.criteria) > 0 and self.no_ddp_model.norm_pix_loss:
+                if self.criteria is not None and len(self.criteria) > 0 and self.no_ddp_model.norm_pix_loss:
                     # y = rearrange(y, '(b t) c h w -> b c t h w', b=bsz)
                     if 'recon_loss' in self.criteria.keys():
                         recon_loss = self.criteria['recon_loss'](g, y)
