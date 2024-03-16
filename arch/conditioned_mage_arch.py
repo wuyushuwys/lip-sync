ccb481fe28124aa802366302c46099cffc64e0dd
wuyushuwys <wuyushuwys@outlook.com>
2024-03-16 16:59:23 -0400

    update AudioPretrainedEncoder

--- arch/conditioned_mage_arch.py
+++ arch/conditioned_mage_arch.py
@@ -17,7 +17,7 @@ from utils.logging_tool import get_logger
 from .mage_basic_arch import MlmLayer, Block, CrossBlock, BertEmbeddings, LabelSmoothingCrossEntropy
 from .fema_vqgan_arch import FaceCoderNet
 from .ref_control_net_arch import RefControlNet
-from .auxiliary_arch import AudioNet, AudioEncoder, AudioPretrainEncoder
+from .auxiliary_arch import AudioNet, AudioEncoder, AudioPretrainedEncoder
 from .ops import PositionalEncoding, get_2d_sincos_pos_embed
 
 
@@ -116,7 +116,7 @@ class DoubleConditionedMAGE(nn.Module):
         self.use_audio_reference = use_audio_reference
         if use_audio_reference:
             # self.audio_net = AudioEncoder(emb_dim=decoder_embed_dim)
-            self.audio_net = AudioPretrainEncoder(audio_weight_path=audio_weight_path, emb_dim=decoder_embed_dim)
+            self.audio_net = AudioPretrainedEncoder(audio_weight_path=audio_weight_path, emb_dim=decoder_embed_dim)
 
         # create image reference mapping that map img ref emb_dim to decoder_embed_dim
         self.use_image_reference = use_image_reference
