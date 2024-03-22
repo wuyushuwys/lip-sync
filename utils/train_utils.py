5611fd23d963c4ac201a7f586fe1122ba192c333
wuyushuwys <wuyushuwys@outlook.com>
2024-03-22 01:56:44 -0400

    update

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -113,7 +113,7 @@ def create_criteria(args: argparse.Namespace):
     if args.losses is not None:
         for name, kwargs in args.losses.items():
             if 'type' in kwargs:
-                logger.info(f"Create {name}[{kwargs.get({type})}]")
+                logger.info(f"Create {name}[{kwargs.get({'type'})}]")
                 loss = getattr(losses_module, kwargs.get('type'))
                 criteria[name] = loss(**subdict(kwargs, 'type')).to(args.local_rank)
 
