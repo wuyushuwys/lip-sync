c7382f8ebf4ab68ed36814fe961d35ad50f778f8
wuyushuwys <wuyushuwys@outlook.com>
2024-06-30 21:12:00 -0700

    temporal decoder

--- utils/io.py
+++ utils/io.py
@@ -9,7 +9,7 @@ def export_fig(frames, output_file, fps=5):
         .input('pipe:',
                format='rawvideo',
                pix_fmt='rgb24',
-               s='{}x{}'.format(h, w),
+               s='{}x{}'.format(w, h),
                filter_complex="[0]split[a][b]; [a]palettegen[palette]; [b][palette]paletteuse",
                r=fps,
                thread_queue_size=1024)
