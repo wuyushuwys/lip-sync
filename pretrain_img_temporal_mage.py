52cdcd0b97e81d910d9b0ad568dcfcc3ca563792
wuyushuwys <wuyushuwys@outlook.com>
2024-10-04 14:08:50 -0400

    remove evaluation

--- pretrain_img_temporal_mage.py
+++ pretrain_img_temporal_mage.py
@@ -83,10 +83,10 @@ def main(args):
 
     logger.info(attr_extractor(args))
 
-    if args.weight or args.ckpt:
-        trainer.evaluating_epoch(epoch=start_epoch)
-        if args.eval_only:
-            return logger.info('Finish evaluation')
+    # if args.weight or args.ckpt:
+        # trainer.evaluating_epoch(epoch=start_epoch)
+        # if args.eval_only:
+        #     return logger.info('Finish evaluation')
 
     for epoch in range(start_epoch + 1, args.epochs + 1):
         # Train
